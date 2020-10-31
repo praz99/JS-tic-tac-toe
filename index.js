b96bee529dfdaf699b5fcc128e98931dd0b172f1
praz <t.prazwal@gmail.com>
2020-10-31 14:18:33 +0100

    add logic to switch current players

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const X_CLASS = 'x';
 const CIRCLE_CLASS = 'circle';
 const cellElements = document.querySelectorAll('[data-cell]');
+const board = document.getElementById('board');
 let circleTurn;
 
 cellElements.forEach(cell => {
@@ -11,8 +12,24 @@ function handleClick(e) {
   const cell = e.target
   const currentClass = circleTurn ? CIRCLE_CLASS :X_CLASS;
   placeMark(cell, currentClass);
+  swapTurns();
+  setBoardHoverClass();
 };
 
 function placeMark(cell, currentClass) {
   cell.classList.add(currentClass);
-}
+};
+
+function swapTurns() {
+  circleTurn = !circleTurn;
+};
+
+function setBoardHoverClass() {
+  board.classList.remove(X_CLASS);
+  board.classList.remove(CIRCLE_CLASS);
+  if(circleTurn) {
+    board.classList.add(CIRCLE_CLASS);
+  } else {
+    board.classList.add(X_CLASS);
+  }
+};
