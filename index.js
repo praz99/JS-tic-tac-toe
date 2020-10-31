20bd32f61a46bb7e0e88d22bcf224a39381c25d7
praz <t.prazwal@gmail.com>
2020-10-31 14:59:24 +0100

    add logic to check and display winner

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const X_CLASS = 'x';
 const CIRCLE_CLASS = 'circle';
+const WINNING_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
 const cellElements = document.querySelectorAll('[data-cell]');
 const board = document.getElementById('board');
+const winningMessageElement = document.getElementById('winning-message');
+const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
 let circleTurn;
 
 startGame();
@@ -16,10 +28,25 @@ function startGame() {
 
 function handleClick(e) {
   const cell = e.target
-  const currentClass = circleTurn ? CIRCLE_CLASS :X_CLASS;
+  const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
   placeMark(cell, currentClass);
-  swapTurns();
-  setBoardHoverClass();
+  if(checkWin(currentClass)) {
+    endGame(false);
+  } else if(isDraw()) {
+    endGame(true);
+  } else {
+    swapTurns();
+    setBoardHoverClass();
+  }
+};
+
+function endGame(draw) {
+  if(draw) {
+    winningMessageElement.innerText = 'Draw!';
+  } else {
+    winningMessageTextElement.innerText = `${circleTurn ? "O" : "X"} Wins!`
+  }
+  winningMessageElement.classList.add('show');
 };
 
 function placeMark(cell, currentClass) {
@@ -38,4 +65,12 @@ function setBoardHoverClass() {
   } else {
     board.classList.add(X_CLASS);
   }
+};
+
+function checkWin(currentClass) {
+  return WINNING_COMBINATIONS.some(combination => {
+    return combination.every(index => {
+      return cellElements[index].classList.contains(currentClass);
+    });
+  });
 };
