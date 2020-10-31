b3ab3062b9066fc87581dab840611e01a13ade52
praz <t.prazwal@gmail.com>
2020-10-31 15:08:52 +0100

    add logic to check draw and restart game

--- index.js
+++ index.js
@@ -13,17 +13,24 @@ const WINNING_COMBINATIONS = [
 const cellElements = document.querySelectorAll('[data-cell]');
 const board = document.getElementById('board');
 const winningMessageElement = document.getElementById('winning-message');
+const restartButton = document.getElementById('restart-button')
 const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
 let circleTurn;
 
 startGame();
 
+restartButton.addEventListener('click', startGame);
+
 function startGame() {
   circleTurn = false;
   cellElements.forEach(cell => {
+    cell.classList.remove(X_CLASS);
+    cell.classList.remove(CIRCLE_CLASS);
+    cell.removeEventListener('click', handleClick);
     cell.addEventListener('click',handleClick, { once: true });
   });
   setBoardHoverClass();
+  winningMessageElement.classList.remove('show');
 };
 
 function handleClick(e) {
@@ -42,12 +49,18 @@ function handleClick(e) {
 
 function endGame(draw) {
   if(draw) {
-    winningMessageElement.innerText = 'Draw!';
+    winningMessageTextElement.innerText = 'Draw!';
   } else {
     winningMessageTextElement.innerText = `${circleTurn ? "O" : "X"} Wins!`
   }
   winningMessageElement.classList.add('show');
 };
+
+function isDraw() {
+  return [...cellElements].every(cell => {
+    return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS);
+  })
+}
 
 function placeMark(cell, currentClass) {
   cell.classList.add(currentClass);
